9425dbb0421e3f52d3a924ac85c8a73d9449ed4f
roamiiing <unf.hwm@ya.ru>
2021-04-04 18:05:44 +0700

    Error handling and logging

--- server/src/api/game.ts
+++ server/src/api/game.ts
@@ -4,19 +4,25 @@ import client from '../kgs/client';
 const game = async (req: Request, res: Response, next: NextFunction) => {
   const { timestamp } = req.params;
 
-  const id = await client.getGame(timestamp);
+  try {
+    const id = await client.getGame(timestamp);
 
-  const { gameDeliverer } = client;
-  gameDeliverer.wait(id, () => {
-    // We listen for ANY data in gameDeliver container because KGS
-    // responds without an unique ID for the game
-    const data = gameDeliverer.receive(id);
-    if (data) {
-      res.json(data);
-    } else {
-      next(new Error(`Cannot get game with timestamp ${timestamp}`));
-    }
-  });
+    const { gameDeliverer } = client;
+    gameDeliverer.wait(id, () => {
+      // We listen for ANY data in gameDeliver container because KGS
+      // responds without an unique ID for the game
+      const data = gameDeliverer.receive(id);
+      if (data) {
+        res.json(data);
+      } else {
+        res.status(500);
+        next(new Error(`Cannot get game with timestamp ${timestamp}`));
+      }
+    });
+  } catch (e) {
+    res.status(500);
+    next(new Error(`Cannot get game with timestamp ${timestamp}`));
+  }
 };
 
 export default game;
