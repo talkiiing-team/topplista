753df9d0eb1553ad0567e08576735d36fce34717
roamiiing <unf.hwm@ya.ru>
2021-04-06 11:04:48 +0700

    Fix url

--- client/src/core/kgs/client.ts
+++ client/src/core/kgs/client.ts
@@ -11,8 +11,8 @@ export default class KGSClient {
 
   constructor() {
     this.instance = axios.create({
-      // baseURL: 'https://walfz.cloud.nstu.ru/kgs/api/',
-      baseURL: 'http://localhost:8080/api/',
+      baseURL: 'https://walfz.cloud.nstu.ru/kgs/api/',
+      // baseURL: 'http://localhost:8080/api/',
     })
   }
 
